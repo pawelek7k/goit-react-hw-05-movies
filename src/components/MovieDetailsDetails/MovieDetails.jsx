8682c2ea3f0320bd751751956b022b0ef4f3c9e9
pawelek <k.pawelek@vp.pl>
2024-05-07 22:50:33 +0200

    add fixes

--- src/components/MovieDetailsDetails/MovieDetails.jsx
+++ src/components/MovieDetailsDetails/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { SectionText } from "../SectionText/Sectiontext";
 
 export const MovieDetails = ({ apiKey }) => {
   const [movie, setMovie] = useState(null);
@@ -38,17 +39,29 @@ export const MovieDetails = ({ apiKey }) => {
   }
 
   return (
-    <div>
-      <h2>{movie.title}</h2>
-      {movie.backdrop_path && (
-        <img
-          src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
-          alt=""
-          width="400"
-        />
-      )}
-      <p>{movie.overview}</p>
-    </div>
+    <>
+      <div>
+        <SectionText text={movie.title} />
+        {movie.backdrop_path && (
+          <img
+            src={`https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`}
+            alt=""
+            width="400"
+          />
+        )}
+        <SectionText text="Overview" />
+        <p>{movie.overview}</p>
+        <SectionText text="Genres" />
+        <ul>
+          {movie.genres.map((genre) => (
+            <li key={genre.id}>{genre.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <SectionText text="Additional Information" />
+      </div>
+    </>
   );
 };
 
