1a37d3139b6e75c2dd73b341d2737b646e3e7b6c
pawelek <k.pawelek@vp.pl>
2024-05-07 10:53:23 +0200

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Header } from "./components/header/Header";
 
 function App() {
-  return <></>;
+  let API_KEY = "b66d75e8f7862c194f0cbd7322865cc6";
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 }
 
 export default App;
