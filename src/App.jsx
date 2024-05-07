1447ea03552383281b855c9533377a0ef0553d55
pawelek <k.pawelek@vp.pl>
2024-05-07 11:07:07 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { ApiSection } from "./components/ApiSection/ApiSection";
-import { Header } from "./components/Header/Header";
+import { Header } from "./components/HeaderComp/Header";
 
 function App() {
   // let API_KEY = "b66d75e8f7862c194f0cbd7322865cc6";
